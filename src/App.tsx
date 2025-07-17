ca8b07e2d91cfdbac412956af99a91812044c76a
choneface <gavin.garcia0001@gmail.com>
2025-07-17 16:28:37 -0700

    enforce the width remains constant throughout the experience

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div
+      style={{ width: "50vw", minWidth: 320, maxWidth: 900, margin: "0 auto" }}
+    >
       <Header />
       <SpeedReader />
       <Footer />
-    </>
+    </div>
   );
 }
 
